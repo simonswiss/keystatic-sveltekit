7a370d29899057e62f4d4f304bcc27a09f880196
Simon Vrachliotis <simon@simonswiss.com>
2023-12-19 14:34:09 +1100

    prerender pages

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,6 +1,8 @@
 import { error } from '@sveltejs/kit';
 import { reader } from '$lib/reader';
 
+export const prerender = true;
+
 export async function load() {
 	const posts = await reader.collections.posts.all({ resolveLinkedFiles: true });
 
--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -1,6 +1,8 @@
 import { error } from '@sveltejs/kit';
 import { reader } from '$lib/reader';
 
+export const prerender = true;
+
 export async function load({ params }) {
 	const post = await reader.collections.posts.read(params.slug, { resolveLinkedFiles: true });
 
